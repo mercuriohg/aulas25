49ff6439c4e672257c5e3f4bf12e4f839f58fffc
Arthur Gomes <153852894+mercuriohg@users.noreply.github.com>
2025-04-13 23:31:52 -0300

    Update ex001.sql

--- bancodados/Exercícios/Ex001/ex001.sql
+++ bancodados/Exercícios/Ex001/ex001.sql
@@ -19,5 +19,11 @@ SELECT nome,salario from jogador WHERE salario = (SELECT MAX(salario) FROM jogad
 --7.Listar o nome dos jogadores com o nome dos seus clubes.
 SELECT jogador.nome as jogador,clube.nome as clube from jogador join clube on jogador.codclube = clube.codclube;
 
+--8."Excluir" todos os registros de campeonatos inferiores a 2007.
+SELECT ano from campeonato where ano < 2007;
 
+--9.Listar as cidades e os nomes dos clubes dessas cidades.
+SELECT cidade.nome as cidade, clube.nome as clube from cidade join clube on cidade.codcidade = clube.codclube;
 
+--10.Listar o nome e posicao dos jogadores do clube "Gremio".
+SELECT jogador.nome as jogador,posicao, clube.nome as clube from jogador join clube on jogador.codjogador = clube.codclube WHERE clube.codclube = 1;
