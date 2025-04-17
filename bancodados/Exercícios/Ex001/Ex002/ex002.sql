9fd3193cb178b4bd980970582f03a0088f33ceb0
Arthur Gomes <153852894+mercuriohg@users.noreply.github.com>
2025-04-17 18:10:33 -0300

    Delete bancodados/Exercícios/Ex001/Ex002 directory

--- bancodados/Exercícios/Ex001/Ex002/ex002.sql
+++
@@ -1,8 +0,0 @@
---Retornar os nomes dos alunos que fazem o curso de Administração 
-select aluno from aluno where cod_curso = 1;
-
---Retornar todos os dados dos alunos.
-SELECT * from aluno;
-
---Retornar todos os dados dos cursos.
-select * from curso;
