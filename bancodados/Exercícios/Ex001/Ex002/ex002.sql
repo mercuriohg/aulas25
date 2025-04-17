6929b11eb5231976798f12b9005039fb2d81f9ec
Arthur Gomes <153852894+mercuriohg@users.noreply.github.com>
2025-04-17 09:34:23 -0300

    Update ex002.sql

--- bancodados/Exercícios/Ex001/Ex002/ex002.sql
+++ bancodados/Exercícios/Ex001/Ex002/ex002.sql
@@ -1,1 +1,8 @@
+--Retornar os nomes dos alunos que fazem o curso de Administração 
+select aluno from aluno where cod_curso = 1;
 
+--Retornar todos os dados dos alunos.
+SELECT * from aluno;
+
+--Retornar todos os dados dos cursos.
+select * from curso;
