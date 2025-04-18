b61c643c454f5440dbb63ea8012602d162072579
Arthur Gomes <153852894+mercuriohg@users.noreply.github.com>
2025-04-18 15:52:50 -0300

    Update ex002.sql

--- bancodados/Exercícios/Ex002/ex002.sql
+++ bancodados/Exercícios/Ex002/ex002.sql
@@ -6,3 +6,26 @@ SELECT * from aluno;
 
 --Retornar todos os dados dos cursos.
 select * from curso;
+
+--Retornar a quantidade de alunos que fazem o curso de Informática.
+select count(nome_aluno) from aluno where cod_curso = 2;
+
+--Retornar a quantidade de alunos que fazem o curso de Informática OU Administração.
+select count(nome_aluno) from aluno where cod_curso = 1 or cod_curso = 2;
+
+--Retornar a quantidade de alunos que NÃO fazem o curso de Informática OU Administração.
+select count(nome_aluno) from aluno where cod_curso != 1 and cod_curso != 2;
+
+--Retornar a quantidade de alunos por curso.
+SELECT c.nome_curso, COUNT(a.cod_aluno) AS quantidade_alunos
+FROM curso c
+LEFT JOIN aluno a ON c.cod_curso = a.cod_curso
+GROUP BY c.nome_curso
+ORDER BY quantidade_alunos DESC;
+
+--Retornar a quantidade de alunos por curso ordenando em ordem crescente pela quantidade de alunos por curso.
+SELECT c.nome_curso, COUNT(a.cod_aluno) AS quantidade_alunos
+FROM curso c
+LEFT JOIN aluno a ON c.cod_curso = a.cod_curso
+GROUP BY c.nome_curso
+ORDER BY quantidade_alunos ASC;
