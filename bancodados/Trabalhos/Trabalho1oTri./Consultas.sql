4805371dd757b7f33d34c39d9e0b47f0105aec26
Arthur Gomes <153852894+mercuriohg@users.noreply.github.com>
2025-05-19 21:34:08 -0300

    Update Consultas.sql

--- bancodados/Trabalhos/Trabalho1oTri./Consultas.sql
+++ bancodados/Trabalhos/Trabalho1oTri./Consultas.sql
@@ -1,10 +1,4 @@
 --Consulta 1: Deve envolver as cláusulas Select – From – Where. A consulta deve envolver no mínimo duas tabelas e fazer sentido.
-SELECT 
-   nome, escola
-from 
-   estudante 
-where 
-   codescola = 1;
 
 --Consulta 2: Deve envolver as cláusulas Select – From – Where. Devem ser consultadas no mínimo 3 tabelas, a consulta deve fazer algum sentindo para o contexto escolhido.
 
