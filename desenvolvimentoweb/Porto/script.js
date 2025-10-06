201d36a46f08de5d63235b2dcc7f6ed0ee5a5b94
Arthur Gomes <153852894+mercuriohg@users.noreply.github.com>
2025-10-06 11:24:26 -0300

    Add files via upload

--- desenvolvimentoweb/Porto/script.js
+++ desenvolvimentoweb/Porto/script.js
@@ -1,7 +1,18 @@
 $(document).ready(function () {
     $("#buttonins").click(function () {
         item = $("#caixadeform").val()
-        $("#lista").append('<li>' +item+'</li>')
-
+        $("#lista").append("<li>" +item+ "<a href=''> Remover </a></li>")
+        contador = $("#lista li").length
+        $("#quant").text(contador)
+        
+        
+    })
+    //Seletor descendente para pegar a tag de link "a" no id de lista.
+    //ON funciona como onclick
+    $("#lista").on("click", "a", function(e){
+      e.preventDefault();   
+      $(this).parent().remove()
+      quant = $("#lista li").length
+      $("#quant").text(quant    )
     })
 })
