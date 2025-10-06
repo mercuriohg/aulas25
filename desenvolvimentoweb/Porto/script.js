8d197caac93e7b716bd220f562edb3fea9316887
Arthur Gomes <153852894+mercuriohg@users.noreply.github.com>
2025-10-06 13:58:05 -0300

    Update script.js

--- desenvolvimentoweb/Porto/script.js
+++ desenvolvimentoweb/Porto/script.js
@@ -1,18 +1,41 @@
+// Aguarda todo o documento (HTML) ser carregado antes de executar o script
 $(document).ready(function () {
+
+    // Quando o botão com id "buttonins" for clicado...
     $("#buttonins").click(function () {
+
+        // Captura o valor digitado na caixa de texto com id "caixadeform"
         item = $("#caixadeform").val()
-        $("#lista").append("<li>" +item+ "<a href=''> Remover </a></li>")
+
+        // Adiciona um novo <li> na lista <ul id="lista">,
+        // contendo o texto digitado e um link "Remover"
+        $("#lista").append("<li>" + item + "<a href=''> Remover </a></li>")
+
+        // Conta quantos <li> existem dentro da lista (quantidade de itens)
         contador = $("#lista li").length
+
+        // Atualiza o número dentro da tag <span id="quant"> com a quantidade atual de itens
         $("#quant").text(contador)
-        
-        
     })
-    //Seletor descendente para pegar a tag de link "a" no id de lista.
-    //ON funciona como onclick
+
+    // -----------------------------------------------
+    // Aqui vem a parte responsável por remover itens
+    // -----------------------------------------------
+
+    // Usa o método .on() para "escutar" cliques em links <a> dentro da lista (#lista)
+    // Mesmo que esses links sejam adicionados dinamicamente
     $("#lista").on("click", "a", function(e){
-      e.preventDefault();   
-      $(this).parent().remove()
-      quant = $("#lista li").length
-      $("#quant").text(quant    )
+
+        // Impede o comportamento padrão do link (recarregar a página)
+        e.preventDefault();
+
+        // Remove o elemento <li> pai do link clicado
+        $(this).parent().remove()
+
+        // Reconta quantos <li> ainda restam na lista
+        quant = $("#lista li").length
+
+        // Atualiza o contador novamente no <span id="quant">
+        $("#quant").text(quant)
     })
 })
