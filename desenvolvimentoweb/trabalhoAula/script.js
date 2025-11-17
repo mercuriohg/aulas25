9620e5cdfadc0888c5a6949d38c79573ac920864
Arthur Gomes <153852894+mercuriohg@users.noreply.github.com>
2025-11-17 11:22:37 -0300

    Add files via upload

--- desenvolvimentoweb/trabalhoAula/script.js
+++ desenvolvimentoweb/trabalhoAula/script.js
@@ -1,9 +1,47 @@
-$(document).ready(function(){
-    $('#addprod').click(function(){
-        arq = $('#arquivo').val()
-        item1 = $('#nome-produto').val()
-        item2 = $('#botao').val()
+$(document).ready(function () {
+    $('#addprod').click(function () {
 
-        $("#lista-itens").append(`<li> <img src='${arq}'>` +  item1 + item2  + `<a href=''> Remover </a></li>`)
+        // pega os valores
+        let nome = $('#nome-produto').val()
+        let preco = $('#botao').val()
+        let fileInput = $('#arquivo')[0]
+
+        // valida imagem
+        if (fileInput.files.length === 0) {
+            alert("Selecione uma imagem!")
+            return;
+        }
+
+        let file = fileInput.files[0]
+        let reader = new FileReader()
+
+        reader.onload = function (e) {
+
+            let imgURL = e.target.result; // base64 da imagem
+
+            // adiciona o item no UL
+            $("#lista-itens").append(`
+            <li>
+                <p>
+                    <img src="${imgURL}" width="100"><br>
+                    Nome: ${nome}<br>
+                    Preço: R$ ${preco}<br>
+                    <a href="#" class="remover">Remover</a>
+                </p>
+            </li>
+            `
+            )   
+           /* $('#nome-produto').val('')
+            $('#botao').val('')
+            $('#arquivo').val('')*/
+        }
+
+        reader.readAsDataURL(file)
+    })
+
+    // Remover item
+    $('#lista-itens').on('click', '.remover', function (e) {
+        e.preventDefault();
+        $(this).parent().remove();
     })
 })
